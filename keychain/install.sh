cccbb2c727afa58e18e891265e516b434cc7bb2d
Felix Kästner <mail@felix-kaestner.com>
2021-12-22 19:40:59 +0100

    remove keychain

--- keychain/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sSLO https://github.com/funtoo/keychain/archive/refs/tags/2.8.5.zip
-unzip -qq -o 2.8.5.zip
-sudo mv keychain-2.8.5/keychain /usr/bin/
-sudo mv keychain-2.8.5/keychain.1 /usr/share/man/man1/
-rm -rf 2.8.5.zip keychain-2.8.5
