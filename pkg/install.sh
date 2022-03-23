a5578f093b8a0fb20bdbbdf3599d39d143478b6f
Felix Kästner <mail@felix-kaestner.com>
2022-03-23 10:09:08 +0100

    formatting

--- pkg/install.sh
+++ pkg/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -x "$(command -v winget.exe)" ] ; then
+if [ -x "$(command -v winget.exe)" ]; then
     DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
     "${WINGET}" import -i "${DIR}/pkgs.json"
 fi
