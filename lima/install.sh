38cb44dbec584780e5c5ca73b67571b4eea1ea35
Felix Kästner <mail@felix-kaestner.com>
2023-04-26 19:19:46 +0200

    refactor installers

--- lima/install.sh
+++ lima/install.sh
@@ -2,15 +2,11 @@
 
 case $(uname -s) in
 'Linux')
-	if [ -f /usr/local/bin/lima ]; then
-		echo "lima is already installed. Skipping..."
-	else
-		VERSION=$(curl -fsSL https://api.github.com/repos/lima-vm/lima/releases/latest | jq -r .tag_name)
-		curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzvm /usr/local
-	fi
-	;;
+    VERSION=$(curl -fsSL https://api.github.com/repos/lima-vm/lima/releases/latest | jq -r .tag_name)
+    curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzvm /usr/local
+    ;;
 'Darwin')
-	brew install lima
-	;;
+    brew install lima
+    ;;
 *) ;;
 esac
--- minikube/install.sh
+++ minikube/install.sh
@@ -2,16 +2,24 @@
 
 case $(uname -s) in
 'Linux')
-	if [ -f /usr/local/bin/minikube ]; then
-		echo "minikube is already installed. Skipping..."
-	else
-		curl -fsSLO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-$(dpkg --print-architecture)"
-		sudo install "minikube-linux-$(dpkg --print-architecture)" /usr/local/bin/minikube
-		rm -rf "minikube-linux-$(dpkg --print-architecture)"
-	fi
-	;;
+    ARCH_NAME="$(uname -m)"
+    case ${ARCH_NAME##*-} in
+    x86_64) OS_ARCH_SUFFIX=amd64 ;;
+    aarch64) OS_ARCH_SUFFIX=arm64 ;;
+    armv7l) OS_ARCH_SUFFIX=arm ;;
+    ppc64le) OS_ARCH_SUFFIX=ppc64le ;;
+    s390x) OS_ARCH_SUFFIX=s390x ;;
+    *)
+        echo >&2 "error: unsupported architecture: '${ARCH_NAME}'"
+        exit 1
+        ;;
+    esac
+    curl -fsSLO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-${OS_ARCH_SUFFIX}"
+    sudo install "minikube-linux-${OS_ARCH_SUFFIX}" /usr/local/bin/minikube
+    rm -rf "minikube-linux-${OS_ARCH_SUFFIX}"
+    ;;
 'Darwin')
-	brew install minikube
-	;;
+    brew install minikube
+    ;;
 *) ;;
 esac
