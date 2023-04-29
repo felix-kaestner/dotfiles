acf001d20865403e32674aa857cd3834782483fa
Felix Kästner <mail@felix-kaestner.com>
2023-04-29 17:17:12 +0200

    non-interactive install scripts

--- vscode/install.sh
+++ vscode/install.sh
@@ -10,15 +10,15 @@ case $(uname -s) in
             sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
             sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
             rm -f packages.microsoft.gpg
-            sudo apt install apt-transport-https
+            sudo apt install -y apt-transport-https
             sudo apt update
-            sudo apt install code
+            sudo apt install -y code
             ;;
         'fedora')
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
             dnf check-update
-            sudo dnf install code
+            sudo dnf install -y code
             ;;
         *) ;;
         esac
