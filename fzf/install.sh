e1f2b832d4d1541a3443919c42641687f2b5c480
Felix Kästner <mail@felix-kaestner.com>
2023-04-29 13:28:37 +0200

    add fzf install

---
+++ fzf/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+case $(uname -s) in
+'Linux')
+	[ -x "$(command -v apt)" ] && sudo apt install -y fzf
+	[ -x "$(command -v dnf)" ] && sudo dnf install -y fzf
+	;;
+'Darwin')
+	brew install fzf
+	;;
+*) ;;
+esac
