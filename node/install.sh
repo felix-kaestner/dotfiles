8eea8ce7e34263d5029c63b4b4b9983896f5f0e3
Felix Kästner <mail@felix-kaestner.com>
2023-07-22 18:45:39 +0200

    install Node.js using pnpm env

--- node/install.sh
+++ node/install.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
 
-case $(uname -s) in
-'Linux')
-	if [ -f /etc/os-release ]; then
-		case $(awk -F= '/^ID=/{print $2}' /etc/os-release) in
-		'debian' | 'ubuntu')
-			curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-			sudo apt install -y nodejs
-			sudo npm install -g yarn
-			sudo npm install -g pnpm
-			;;
-		'fedora')
-			sudo dnf install -y nodejs
-			sudo npm install -g yarn
-			sudo npm install -g pnpm
-			;;
-		*) ;;
-		esac
-	fi
-	;;
-'Darwin')
-	brew install node yarn pnpm
-	;;
-*) ;;
-esac
+wget -qO- https://get.pnpm.io/install.sh | sh - > /dev/null
+sed -i '/#\ pnpm/,/#\ pnpm\ end/d' ~/.zshrc
+export PNPM_HOME="$HOME/.local/share/pnpm"
+pnpm env use --global lts
+
