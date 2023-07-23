10908c778c3a6ddab6ec4167a4f8244536ca4d94
Felix Kästner <mail@felix-kaestner.com>
2023-07-23 13:08:43 +0200

    refactor node/pnpm install script

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-wget -qO- https://get.pnpm.io/install.sh | sh - > /dev/null
-sed -i '/#\ pnpm/,/#\ pnpm\ end/d' ~/.zshrc
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
+
+wget -qO- https://get.pnpm.io/install.sh | sh -
+git checkout HEAD -- "$DIR/zshrc.symlink"
 export PNPM_HOME="$HOME/.local/share/pnpm"
 pnpm env use --global lts
 
