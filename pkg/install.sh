e0fd4ce91ed0776fb57c6455d3dbeddafc43888d
Felix Kästner <mail@felix-kaestner.com>
2023-08-16 21:10:49 +0200

    remove Windows and WSL related stuff

--- pkg/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x "$(command -v winget.exe)" ]; then
-    DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-    "${WINGET}" import -i "${DIR}/pkgs.json"
-fi
