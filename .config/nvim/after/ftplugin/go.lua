6e6e87fd95c7ccc633e799a4550ecf60aea6d237
Felix Kästner <mail@felix-kaestner.com>
2023-08-20 19:53:13 +0200

    fix go test keymap

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,3 +1,3 @@
 -- Remap to quickly run tests in Go
-vim.keymap.set("n", "<leader>gt", "<cmd>!go test -v %:p:h<cr>", { buffer = bufnr, desc = "[G]o [T]est" })
+vim.keymap.set("n", "<leader>gt", "<cmd>!go test -v %:p:h<cr>", { desc = "[G]o [T]est" })
 
