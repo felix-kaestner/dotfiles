1b1c6414b8cb7c3a451b3836eb228c332ea9c2e6
Felix Kästner <mail@felix-kaestner.com>
2023-08-20 20:36:31 +0200

    fix node.js install script

--- node/install.sh
+++ node/install.sh
@@ -2,8 +2,9 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
 
-wget -qO- https://get.pnpm.io/install.sh | sh -
+curl -sSL https://get.pnpm.io/install.sh | sh -
 git checkout HEAD -- "$DIR/zshrc.symlink"
 export PNPM_HOME="$HOME/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
 pnpm env use --global lts
 
