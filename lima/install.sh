44c94f2bcc2cebf7cd23a92abe8705257fac7f54
Felix Kästner <mail@felix-kaestner.com>
2023-10-28 15:31:46 +0200

    fix lima install

--- lima/install.sh
+++ lima/install.sh
@@ -2,8 +2,9 @@
 
 case $(uname -s) in
 'Linux')
+    sudo rm -rf /usr/local/bin/*lima* /usr/local/lima
     VERSION=$(curl -fsSL https://api.github.com/repos/lima-vm/lima/releases/latest | jq -r .tag_name)
-    curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzvm /usr/local
+    curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzm /usr/local --exclude='./share/lima/examples'
     ;;
 'Darwin')
     brew install lima
