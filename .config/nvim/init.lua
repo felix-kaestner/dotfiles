472fc46bcef6918cd5218c2643429b584812a57d
Felix Kästner <felix.kaestner@aboutyou.com>
2024-01-21 19:13:23 +0100

    chore(neovim): add more which_key prefixes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -653,7 +653,9 @@ require("lazy").setup({
             require("which-key").setup(opts)
             require("which-key").register({
                 ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+                ["<leader>f"] = { name = "[F]ile", _ = "which_key_ignore" },
                 ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+                ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
                 ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
                 ["<leader>h"] = { name = "[H]unk", _ = "which_key_ignore" },
                 ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
