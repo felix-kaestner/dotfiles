d3a501fe41c5c3d3bfa6a655ff18d2ab1dca05a7
Felix Kästner <felix.kaestner@aboutyou.com>
2024-01-22 10:08:07 +0100

    feat(neovim): add remap to report test coverage of a go package

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,2 +1,18 @@
 -- Remap to quickly run tests in Go
 vim.keymap.set("n", "<leader>gt", "<cmd>!go test -v %:p:h<cr>", { desc = "[G]o [T]est" })
+
+-- Remap to quickly report test coverage in Go
+vim.keymap.set("n", "<leader>tc", function()
+    local Job = require("plenary.job")
+
+    Job:new({
+        command = "go",
+        args = { "test", "-tags=test", "-coverprofile=c.out", vim.fn.expand("%:p:h") },
+    }):sync()
+
+    Job:new({
+        command = "go",
+        args = { "tool", "cover", "-html=c.out" },
+    }):sync()
+end, { desc = "[G]o [C]overage" })
+
