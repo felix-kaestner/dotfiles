76dd286818afa54644667ee7d54a1fe6d0c256ed
Felix Kästner <felix.kaestner@aboutyou.com>
2024-01-23 19:21:02 +0100

    feat(neovim): add treesitter fold from Neovim core
    This feature is currently only available in the nightly version of
    Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,9 +114,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- [[ Fold using Treesitter ]]
 -- See https://github.com/nvim-treesitter/nvim-treesitter#folding
+vim.opt.foldlevel = 69
 vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 -- [[ LSP ]]
 
