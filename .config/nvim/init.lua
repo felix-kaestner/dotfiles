bcbe42e5b012e826b149f28b72cd2f43febe610f
Felix Kästner <felix.kaestner@aboutyou.com>
2024-10-07 18:05:12 +0200

    refactor(nvim): nvim-dap setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -864,6 +864,7 @@ require("lazy").setup({
         dependencies = {
             {
                 "rcarriga/nvim-dap-ui",
+                dependencies = { "nvim-neotest/nvim-nio" },
                 keys = {
                     { "<F7>", "<cmd>lua require('dapui').toggle()<cr>", desc = "Debug: Show Last Session Result" },
                 },
@@ -871,16 +872,11 @@ require("lazy").setup({
                     require("dapui").setup(opts)
 
                     local dap, dapui = require("dap"), require("dapui")
-                    dap.listeners.after.event_initialized["dapui_config"] = function()
-                        dapui.open()
-                    end
-                    dap.listeners.before.event_terminated["dapui_config"] = function()
-                        dapui.close()
-                    end
-                    dap.listeners.before.event_exited["dapui_config"] = function()
-                        dapui.close()
-                    end
-                end,
+
+                    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+                    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+                    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+                end
             },
 
             {
