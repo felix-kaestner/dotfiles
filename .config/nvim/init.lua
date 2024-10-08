34a43e1b957879a3abe194d8f01443e7d384bd46
Felix Kästner <felix.kaestner@aboutyou.com>
2024-10-08 15:08:07 +0200

    chore(nvim): formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -873,10 +873,10 @@ require("lazy").setup({
 
                     local dap, dapui = require("dap"), require("dapui")
 
-                    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-                    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-                    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-                end
+                    dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+                    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+                    dap.listeners.before.event_exited["dapui_config"] = dapui.close
+                end,
             },
 
             {
