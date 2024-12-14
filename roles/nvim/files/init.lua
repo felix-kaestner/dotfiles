fcfd1fcf257c7671cfd252177fa8ef864c8178d9
Felix Kästner <felix.kaestner@aboutyou.com>
2024-12-14 18:06:03 +0100

    refactor(neovim): terminal shortcut

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -102,13 +102,13 @@ vim.keymap.set("n", "<leader>t", function()
         local buf = vim.api.nvim_win_get_buf(win)
         if vim.bo[buf].buftype == "terminal" then
             vim.api.nvim_set_current_win(win)
-            vim.cmd("startinsert")
+            vim.cmd.startinsert()
             return
         end
     end
 
     vim.cmd("botright terminal")
-    vim.cmd("startinsert")
+    vim.cmd.startinsert()
 end)
 
 -- Open/Close Quicklist
