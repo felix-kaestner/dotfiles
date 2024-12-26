e327ea864007416469448ee81b9ac1f9acc769a0
Felix Kästner <felix.kaestner@aboutyou.com>
2024-12-26 14:57:54 +0100

    refactor(neovim): move organize imports in go files to autocmds

--- roles/nvim/files/lua/config/autocmds.lua
+++ roles/nvim/files/lua/config/autocmds.lua
@@ -18,6 +18,25 @@ vim.api.nvim_create_autocmd("TermOpen", {
     end,
 })
 
+-- Organize imports on save
+-- See: https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local params = vim.lsp.util.make_range_params(0, "utf-8")
+        params["context"] = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+    end,
+})
+
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
     group = vim.api.nvim_create_augroup("lsp-codelens", { clear = true }),
     pattern = { "*.go", "*.mod" },
@@ -31,7 +50,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
 vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("tab-indent", { clear = true }),
     pattern = { "go", "make" },
-    callback = function ()
+    callback = function()
         -- Disable listchars for files with tabs
         vim.opt_local.list = false
     end,
--- roles/nvim/files/lua/plugins/lsp.lua
+++ roles/nvim/files/lua/plugins/lsp.lua
@@ -123,24 +123,6 @@ local on_attach = function(client, bufnr)
             group = augroup,
             buffer = bufnr,
             callback = function()
-                if client.name == "gopls" then
-                    -- Organize imports using the LSP
-                    -- See: https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
-                    ---@diagnostic disable-next-line: missing-parameter
-                    local params = vim.lsp.util.make_range_params()
-                    params["context"] = { only = { "source.organizeImports" } }
-                    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-                    for cid, res in pairs(result or {}) do
-                        for _, r in pairs(res.result or {}) do
-                            if r.edit then
-                                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                                vim.lsp.util.apply_workspace_edit(r.edit, enc)
-                            end
-                        end
-                    end
-                end
-
-                -- Format the buffer using the LSP
                 vim.lsp.buf.format({ async = false, bufnr = bufnr })
             end,
         })
