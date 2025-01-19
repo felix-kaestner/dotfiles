ef992ade47090940d1f76e212b92066e0a9d8bda
Felix Kästner <felix.kaestner@sap.com>
2025-01-19 14:06:34 +0100

    feat(k8s): extend kubectl with custom cluster-name command

--- roles/fish/files/functions/kc.fish
+++ roles/fish/files/functions/kc.fish
@@ -1,3 +1,10 @@
+complete -c kc -a "cluster-name" -d "Display cluster name"
+
 function kc --wraps kubectl --description 'alias kc=kubectl'
-    command kubectl $argv
+    switch $argv[1]
+        case cluster-name
+            command kubectl config view --minify -o jsonpath='{.clusters[].name}'
+        case '*'
+            command kubectl $argv
+    end
 end
