ba127405a63bf13a2720066d6ae8c32fbc2b383b
Felix Kästner <felix.kaestner@sap.com>
2025-02-05 11:42:56 +0100

    feat(fish): define XDG_CACHE_HOME if not set

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -4,6 +4,7 @@ set -gx EDITOR "nvim"
 
 # set XDG base directories
 set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -g XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -q XDG_BIN_HOME; or set -gx XDG_BIN_HOME "$HOME/.local/bin"
 
