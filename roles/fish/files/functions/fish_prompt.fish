dc72c459b00f763ff758a4e4aee5ca70c556167a
Felix Kästner <felix.kaestner@sap.com>
2025-02-17 16:29:41 +0100

    feat(fish): use multiline prompt

--- roles/fish/files/functions/fish_prompt.fish
+++ roles/fish/files/functions/fish_prompt.fish
@@ -37,7 +37,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
+    set -l suffix '❯'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
@@ -57,5 +57,6 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (functions -q fish_ccloud_prompt; and fish_ccloud_prompt) (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) (set_color blue) (fish_kube_prompt) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    echo -s (functions -q fish_ccloud_prompt; and fish_ccloud_prompt) (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) (set_color blue) (fish_kube_prompt) $normal (fish_vcs_prompt) $normal " " $prompt_status
+    echo -n -s $suffix " "
 end
