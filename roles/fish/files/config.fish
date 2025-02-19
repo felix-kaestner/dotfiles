bf97b8d07ff6b93fb35e52d4d82d0baffc4b53c8
Felix Kästner <felix.kaestner@sap.com>
2025-02-19 17:42:13 +0100

    feat: override sed on MacOS with GNU sed

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -47,6 +47,11 @@ fish_add_path "$HOME/.symfony/bin"
 # set PATH so it includes user's flutter bin directory if it exists
 fish_add_path "$HOME/.flutter/bin"
 
+# set PATH so it includes the bin directory of gnu-sed if it exists
+if test -x "/opt/homebrew/opt/gnu-sed/libexec/gnubin/sed"
+    fish_add_path "/opt/homebrew/opt/gnu-sed/libexec/gnubin"
+end
+
 test -f "$XDG_CONFIG_HOME/fish/local.fish"; and source "$XDG_CONFIG_HOME/fish/local.fish"
 
 if status is-interactive
