0a9ebde91b7b55bb326a02a83df4f53e9b8b5eab
Felix Kästner <felix.kaestner@sap.com>
2025-02-22 17:29:12 +0100

    refactor(fish): simplify operating system logic in notify utility

--- roles/fish/files/functions/_notify.fish
+++ roles/fish/files/functions/_notify.fish
@@ -4,15 +4,15 @@ function _notify
         return 1
     end
 
-    switch (uname -s)
-        case 'Linux'
+    switch (uname)
+        case Linux
             if type -q notify-send
                 notify-send "$argv[1]" "$argv[2]"
             else
                 echo "notify: unknown notification system" >&2
                 return 1
             end
-        case 'Darwin'
+        case Darwin
             osascript -e "display notification \"$argv[1]\" with title \"$argv[2]\""
         case '*'
             echo "Unsupported operating system: $(uname -s)" >&2
