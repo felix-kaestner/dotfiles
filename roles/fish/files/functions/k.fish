37fa2c3bd3cdcdb1426bad77fd70d3682309ce7b
Felix Kästner <felix.kaestner@sap.com>
2025-03-03 10:57:09 +0100

    feat(k8s): add get-pod-names and get-pod-containers kubectl commands

--- roles/fish/files/functions/k.fish
+++ roles/fish/files/functions/k.fish
@@ -1,9 +1,15 @@
-complete -c k -a "cluster-name" -d "Display cluster name"
+complete -c k -a "cluster-name" -d "Print cluster name"
+complete -c k -a "get-pod-names" -d "Print name(s) of the pod(s) matching the given label selector"
+complete -c k -a "get-pod-containers" -d "Print name(s) of the container(s) within a pod"
 
 function k --wraps kubectl --description 'alias k=kubectl'
     switch $argv[1]
         case cluster-name
             command kubectl config view --minify -o jsonpath='{.clusters[].name}'
+        case get-pod-names
+            command kubectl get pod $argv[2..-1] -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+        case get-pod-containers
+            command kubectl get pod $argv[2..-1] -o jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}'
         case '*'
             command kubectl $argv
     end
