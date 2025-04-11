c2a0b3f6ef2fff2a0a8c30ef6e166ac538e169f1
Felix Kästner <felix.kaestner@sap.com>
2025-04-11 10:32:37 +0200

    refactor(venvwrapper): fzf options

--- roles/fish/files/functions/venv.fish
+++ roles/fish/files/functions/venv.fish
@@ -1,5 +1,5 @@
 function venv --description 'Source a virtualenv'
-    set -l dir (fd -d 1 -t d --base-directory ~/.venvs --path-separator "" | fzf --tmux)
+    set -l dir (fd -d 1 -t d --base-directory ~/.venvs --path-separator "" | fzf --height ~100% --header 'Select a virtualenv to activate in the current shell')
     if test -n $dir; and test -d "$HOME/.venvs/$dir"
         source "$HOME/.venvs/$dir/bin/activate.fish"
     end
