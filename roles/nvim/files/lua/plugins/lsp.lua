403a91bd51b208bfbe975c3b92d0f56f73d0ccd7
Felix Kästner <felix.kaestner@sap.com>
2025-04-19 20:56:18 +0200

    feat(neovim): install codespell linter using mason.nvim

--- roles/nvim/files/lua/plugins/lsp.lua
+++ roles/nvim/files/lua/plugins/lsp.lua
@@ -172,6 +172,7 @@ return {
                 "delve",
                 -- Linter
                 "codespell",
+                "shellcheck",
                 "ruff",
                 -- Formatter
                 "shfmt",
