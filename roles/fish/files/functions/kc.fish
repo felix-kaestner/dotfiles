b14a241d2c39d67e99ff50dd51e5e0b6ca3411fa
Felix Kästner <felix.kaestner@sap.com>
2025-05-01 18:55:44 +0200

    chore(fish): drop kc and kn functions
    Instead use `k ctx` and `k ns`.

--- roles/fish/files/functions/kc.fish
+++
@@ -1,3 +0,0 @@
-function kc --description 'alias kc=kubectx - switch between kubernetes contexts'
-    command kubectl ctx $argv
-end
--- roles/fish/files/functions/kn.fish
+++
@@ -1,3 +0,0 @@
-function kn --description 'alias kn=kubens - switch between kubernetes namespaces'
-    command kubectl ns $argv
-end
