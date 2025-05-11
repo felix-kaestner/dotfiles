262e32d12ceccbf1fb0c8914ea95ac35a3bbdca4
Felix Kästner <felix.kaestner@sap.com>
2025-05-11 16:20:41 +0200

    feat(k): add dbg and dashboard subcommands to kubectl wrapper
    `dbg` is a wrapper around the existing `debug` subcommand, which
    allows to interactively debug a container by adding an ephemeral
    container to an already running pod. This is extended by the
    functionality to automatically select the target container in
    case that the pod only contains a single container and offers
    a fuzzy selection of the target container using fzf in case there
    are multiple containers running in the pod. A `busybox` image is used
    for the debugging container.
    
    `dashboard` is a custom subcommand that launches the [Kubernetes
    Dashboard](https://github.com/kubernetes/dashboard) in case that a
    kubernetes-dashboard service is running in a namespace with the same
    name. It will port-forward the running services port 443 to 8443 on
    localhost and open the resulting url in the default application. If it
    doesn't find a corresponding service it will print the link to the
    installation instructions.
    
    The `get-pod-names` subcommand is removed in favor of using `--output name`.
    
    The `get-pod-containers` subcommand is renamed to just `pod-containers`
    in order to shorten the name and improve auto complete functionality
    after the first few characters have been typed.

--- roles/fish/files/functions/k.fish
+++ roles/fish/files/functions/k.fish
@@ -1,14 +1,36 @@
-complete -c k -a "cluster-name" -d "Print cluster name"
-complete -c k -a "get-pod-names" -d "Print name(s) of the pod(s) matching the given label selector"
-complete -c k -a "get-pod-containers" -d "Print name(s) of the container(s) within a pod"
-
 function k --wraps kubectl --description 'alias k=kubectl'
     switch $argv[1]
+        case dbg
+            set -l pod $argv[2]
+            set -l container (k pod-containers $pod)
+            switch (count $container)
+                case 0
+                    echo "No running container found in pod $pod" >&2
+                    return 1
+                case 1
+                    set container $container[1]
+                case '*'
+                    set container (printf "%s\n" $container | fzf --height ~100% --header 'Select a container to debug' --prompt 'Container> ' || return 130)
+            end
+            command kubectl debug -ti --profile=general --image=busybox:1.37.0 --target="$container" $argv[2..-1]
+        case dashboard
+            if not command kubectl get namespace kubernetes-dashboard &>/dev/null; or not command kubectl get service -n kubernetes-dashboard kubernetes-dashboard-web &>/dev/null
+                echo "Kubernetes dashboard deployment not found. Please follow the instructions on https://github.com/kubernetes/dashboard?tab=readme-ov-file#installation." >&2
+                return 1
+            end
+            command kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 &>/dev/null &
+            set -l pid $last_pid
+            function cleanup --on-signal INT --on-signal TERM --inherit-variable pid
+                kill -9 $pid
+                functions -e cleanup
+                return 130
+            end
+            echo "Serving dashboard on https://localhost:8443. Press Ctrl+C to stop port-forwarding and exit."
+            open "https://localhost:8443"
+            wait $pid
         case cluster-name
             command kubectl config view --minify -o jsonpath='{.clusters[].name}'
-        case get-pod-names
-            command kubectl get pod $argv[2..-1] -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
-        case get-pod-containers
+        case pod-containers
             command kubectl get pod $argv[2..-1] -o jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}'
         case '*'
             command kubectl $argv
